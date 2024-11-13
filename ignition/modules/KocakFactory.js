55ca1764afa68a200276e388a253a6ea6d7f1a4c
salismazaya <salismazaya@gmail.com>
2024-11-13 17:43:19 +0700

    ✨ (KocakFactory.js): update KocakFactory contract initialization to use process.env.OWNER for dynamic ownership configuration

--- ignition/modules/KocakFactory.js
+++ ignition/modules/KocakFactory.js
@@ -1,7 +1,7 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("Kocak", (m) => {
-  const kocak = m.contract("KocakFactory", [0]);
+  const kocak = m.contract("KocakFactory", [process.env.OWNER, 0]);
 
   return { kocak };
 });
